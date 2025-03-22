da75efb499e7f1d7fe43e87e0a6cd827f88974a4
HarshGupta <harshgupta88911@gmail.com>
2025-03-22 20:38:19 +0530

    Fix: Optimize build size and resolve Windows symlink issues

--- plexmeetcall/next.config.ts
+++ plexmeetcall/next.config.ts
@@ -4,6 +4,31 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
   poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    optimizePackageImports: [
+      "@mui/material",
+      "socket.io-client",
+      "react-player",
+      "peerjs",
+    ],
+  },
+  // These packages are being traced but aren't needed in the standalone build
+  // This reduces the size while keeping functionality
+  transpilePackages: [
+    "@swc/core",
+    "react",
+    "react-dom",
+    "@mui/material",
+    "next",
+    "socket.io",
+    "socket.io-client",
+    "peerjs",
+    "simple-peer",
+    "react-player",
+  ],
 };
 
 export default nextConfig;
