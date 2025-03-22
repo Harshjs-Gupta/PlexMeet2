30ba5ecc3a309d97deb3bc79fdcd4fdbf3a289ec
HarshGupta <harshgupta88911@gmail.com>
2025-03-22 20:42:36 +0530

    few changes in next config

--- plexmeetcall/next.config.ts
+++ plexmeetcall/next.config.ts
@@ -1,34 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
-  poweredByHeader: false,
-  compress: true,
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    optimizePackageImports: [
-      "@mui/material",
-      "socket.io-client",
-      "react-player",
-      "peerjs",
-    ],
-  },
-  // These packages are being traced but aren't needed in the standalone build
-  // This reduces the size while keeping functionality
-  transpilePackages: [
-    "@swc/core",
-    "react",
-    "react-dom",
-    "@mui/material",
-    "next",
-    "socket.io",
-    "socket.io-client",
-    "peerjs",
-    "simple-peer",
-    "react-player",
-  ],
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
